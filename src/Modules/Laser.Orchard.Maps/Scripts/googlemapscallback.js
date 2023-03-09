861ef8e1d09e54d78cd597b97f785b391c9f0c65
Andrea Piovanelli <andrea.piovanelli@laser-group.com>
2023-03-09 16:46:08 +0100

    Added $(document).ready() to ensure page is fully loaded before calling Google maps initializer functions.

--- src/Modules/Laser.Orchard.Maps/Scripts/googlemapscallback.js
+++ src/Modules/Laser.Orchard.Maps/Scripts/googlemapscallback.js
@@ -2,9 +2,11 @@
     $("[data-map-provider='google']").each(function () {
         var mapId = $(this).attr("data-map-id");
         if (mapId) {
-            //alert("Found map " + mapId);
             var functionName = "initialize" + mapId;
-            window[functionName]();
+
+            $(document).ready(function () {
+                window[functionName]();
+            });
         }
     });
 }
